8334a3aa96bfa84eb61562fa59dba6ba9cb898a5
Paul Soladoye <psoladoye@gmail.com>
2017-10-16 08:33:41 -0400

    added command char

--- remote-service.js
+++ remote-service.js
@@ -4,13 +4,15 @@ var PrimaryService = bleno.PrimaryService;
 
 var MotorControlCharacteristic = require('./motor-control-characteristic');
 var StepperControlCharacteristic = require('./stepper-control-characteristic');
+var CommandCenterCharacteristic = require('./command-center-characteristic');
 
 function RemoteService() {
   RemoteService.super_.call(this, {
     uuid: '180F',
     characteristics: [
       new MotorControlCharacteristic(),
-      new StepperControlCharacteristic()
+      new StepperControlCharacteristic(),
+      new CommandCenterCharacteristic()
     ]
   });
 }
