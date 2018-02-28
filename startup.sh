48f57acf9f520a0b1c40778a66bd32a3d87cc6a8
Paul Soladoye <psoladoye@gmail.com>
2018-02-28 03:40:38 +0000

    Fixed startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
-#! /bin/sh
-BLE=1 LOG=BALL_FEEDER,CAROUSEL_STEPPER,MAIN,REMOTE_SERVICE,DC_MOTORS,MOTOR_MODEL,AccelStepper,HORIZ_STEPPER,H_STEPPER pm2 start proc_management/pm2-app.json
+#! /bin/bash
+[[ $((pm2 info remote-service || echo 'status|stopped') | grep status) =~ 'stopped' ]] && \
+BLE=1 LOG=BALL_FEEDER,CAROUSEL_STEPPER,MAIN,REMOTE_SERVICE,DC_MOTORS,MOTOR_MODEL,\
+AccelStepper,HORIZ_STEPPER,H_STEPPER pm2 start proc_management/pm2-app.json
